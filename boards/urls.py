dcdf9d35c21180ff76546edb7027fe8c4bcf7e5a
Josh Birch <joshuabirch@protonmail.com>
2026-01-10 15:35:43 +0000

    Implement basic Kanban board creation and display functionality in dashboard

--- boards/urls.py
+++ boards/urls.py
@@ -1,10 +1,13 @@
+# IMPORTS
 from django.urls import path
 
 from . import views
 
+#
 app_name = 'boards'
 
+# URLS
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-
+    path('create/', views.create_board, name='create_board'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,17 +1,33 @@
 # IMPORTS
-## Django
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
 
-## Apps
 from .models import Board
 
 # VIEWS
-##
+class BoardForm(forms.ModelForm):
+    class Meta:
+        model = Board
+        fields = ['title', 'description']
+
 @login_required
 def dashboard(request):
     boards = Board.objects.filter(owner=request.user).order_by('title')
-    return render(request, 'boards/dashboard.html', {'boards': boards})
+    form = BoardForm()
 
+    return render(request, 'boards/dashboard.html', {'boards': boards, 'form': form})
+
+@login_required
+def create_board(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+
+        if form.is_valid():
+            board = form.save(commit=False)
+            board.owner = request.user
+            board.save()
+
+    return redirect('boards:dashboard')
 
